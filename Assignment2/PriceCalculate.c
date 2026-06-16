5dbb20c2005e2431667ea871faaac5209eb68d38
krushna123 <krushnaandhale2246@gmail.com>
2026-06-16 10:20:15 +0530

    some changes in PriceCalculate

--- Assignment2/PriceCalculate.c
+++ Assignment2/PriceCalculate.c
@@ -1,9 +1,19 @@
 #include<stdio.h>
 int main()
+// i also want to add 10 % discount if total price is more than 1000
 {
-    int Quantity;
+    float Quantity,Item_Price,Total_Price;
     printf("enter the Quantity");
-    scanf("%d",&Quantity);
-      
+    scanf("%f",&Quantity);
+    printf("enter the Item Price");
+    scanf("%f",&Item_Price);
+    Total_Price = Quantity * Item_Price;
+    if(Total_Price > 1000)
+    {
+        Total_Price = Total_Price - (Total_Price * 0.1);
+    }
+    
+    printf("Total Price is %f",Total_Price);
+
     return 0;
 }
