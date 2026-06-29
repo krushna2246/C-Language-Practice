af7d85ce3f31f9c528acef9724a430badf4f4ab5
krushna123 <krushnaandhale2246@gmail.com>
2026-06-29 23:38:30 +0530

    pointers concept

---
+++ Assignment10_OnPointers/first.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int a,b;
+    int *p=&b;
+    printf("Enter the values for a & b\n");
+    scanf("%d %d",&a,&b);
+    printf("%u\n",p);
+    printf("%d%d\n",a+b);
+
+
+
+    return 0;
+}
